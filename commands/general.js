9d45a8a6632729588daff7fb0f7c7e52b57245b8
Sahil Makhijani <svmakhijani94@gmail.com>
2022-04-30 16:29:27 -0700

    Added general commands

--- commands/general.js
+++ commands/general.js
@@ -1,5 +1,13 @@
-const general = function general(command) {
-    console.log(`General: ${command}`);
+const general = function general(command, user) {
+    const args = command.split(' ');
+    switch (args[0]) {
+        case '$about':
+            return `Hello! I am SmakTalk94's personal assistant! I'm a work-in-progress, so I can't do much at the moment.`;
+        case '$hi':
+            return (args.length > 1) ? `Hi ${args[1]} <3` : `Hi ${user} <3`;
+        default:
+            return `Command not found`;
+    }
 }
 
 module.exports = general;
