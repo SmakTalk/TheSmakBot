1ea8b7ac9abb2966590ce7a38a891293018e3147
Sahil Makhijani <svmakhijani94@gmail.com>
2022-06-30 02:30:18 -0700

    Wrapped api call in promise

--- api/smakapi.js
+++ api/smakapi.js
@@ -1,36 +1,47 @@
 require('dotenv').config();
 const https = require('https');
 
-const smakapi = function smakapi(data, path, method) {
-    if (data) {
-        const data = JSON.stringify(data);
-    }
-
-    const options = {
-        hostname: process.env.API_URL,
-        path: path,
-        method: method,
-        headers: {
-            'Secret': process.env.SECRET
+const smakapi = (data, path, method) => {
+    return new Promise((resolve, reject) => {
+        if (data) {
+            const data = JSON.stringify(data);
         }
-    };
 
-    const req = https.request(options, (res) => {
-        console.log(`statusCode: ${res.statusCode}`);
+        const options = {
+            hostname: process.env.API_URL,
+            path: path,
+            method: method,
+            headers: {
+                'Secret': process.env.SECRET
+            }
+        };
 
-        res.on('data', (d) => {
-            process.stdout.write(d);
+        const req = https.request(options, (res) => {
+            let chunk = '';
+            console.log(`statusCode: ${res.statusCode}`);
+    
+            res.on('data', (d) => {
+                chunk += d;
+            });
+
+            res.on('end', () => {
+                try {
+                    resolve(chunk);
+                } catch (e) {
+                    reject(e.message);
+                }
+            });
         });
+    
+        req.on('error', (error) => {
+            reject(error);
+        });
+    
+        if (data) {
+            req.write(data);
+        }
+        req.end();
     });
-
-    req.on('error', (error) => {
-        console.error(error);
-    });
-
-    if (data) {
-        req.write(data);
-    }
-    req.end();
 };
 
 module.exports = smakapi;
