cb5d3276965e776316db8b42d038716ed5ce3742
Sahil Makhijani <svmakhijani94@gmail.com>
2023-04-08 19:58:24 -0700

    Switched from tmi to twurple

--- app.js
+++ app.js
@@ -9,59 +9,64 @@ const http = require('./constants/http.js');
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 
-const tokenData = async () => {
-    return JSON.parse(await smakapi('/token', http.GET));
-};
+const main = async () => {
+    const userId = process.env.USER_ID;
 
-const authProvider = async () => {
-    const auth = new RefreshingAuthProvider(
+    const authProvider = new RefreshingAuthProvider(
         {
             clientId,
             clientSecret,
-            onRefresh: async newTokenData => await smakapi('/token', http.POST, newTokenData)
+            onRefresh: async (userId, newTokenData) => await smakapi('/token', http.POST, newTokenData)
         }
     );
-    await auth.addUserForToken(tokenData);
-    return auth;
+    authProvider.addUser(userId, JSON.parse(await smakapi('/token', http.GET)), ['chat']);
+
+    const client = new ChatClient({ authProvider, channels: [ process.env.CHANNEL_NAME ] });
+
+    client.connect();
+
+    // whisperChat(authProvider, client);
+
+    client.onMessage(async (channel, user, text, msg) => {
+        if (user === 'TheSmakBot') { return; }
+      
+        const commandName = text.trim();
+    
+        if (commandName.startsWith('$')) {
+            switch (commandName.split(' ')[0]) {
+                case '$channel':
+                    command.channels(client, channel, commandName, msg.userInfo);
+                    break;
+                case '$enter':
+                    command.entries(client, channel, commandName, msg.userInfo);
+                    break;
+                case '$latest':
+                    await command.latest(client, channel, commandName, msg.userInfo);
+                    break;
+                case '$raid':
+                    command.raids(commandName);
+                    break;
+                case '$drawing':
+                    command.entries(client, channel, commandName, msg.userInfo);
+                    break;
+                case '$streamer':
+                    command.streamers(commandName);
+                    break;
+                default:
+                    command.general(client, channel, commandName, msg.userInfo);
+            }
+        }
+    });
+
+    client.onAuthenticationSuccess(() => {
+        console.log('Authentication successful!');
+    });
+
+    client.onAuthenticationFailure((text, retryCount) => {
+        console.log(text);
+        console.log(retryCount);
+        console.log('------------');
+    });
 };
 
-const client = new ChatClient({ authProvider, channels: [ process.env.CHANNEL_NAME ] });
-
-client.connect();
-
-whisperChat(authProvider, client);
-
-client.onMessage(async (channel, user, text, msg) => {
-    if (user === 'TheSmakBot') { return; }
-  
-    const commandName = text.trim();
-
-    if (commandName.startsWith('$')) {
-        switch (commandName.split(' ')[0]) {
-            case '$channel':
-                command.channels(client, channel, commandName, user);
-                break;
-            case '$enter':
-                command.entries(client, channel, commandName, user);
-                break;
-            case '$latest':
-                await command.latest(client, channel, commandName, user);
-                break;
-            case '$raid':
-                command.raids(commandName);
-                break;
-            case '$drawing':
-                command.entries(client, channel, commandName, user);
-                break;
-            case '$streamer':
-                command.streamers(commandName);
-                break;
-            default:
-                command.general(client, channel, commandName, user);
-        }
-    }
-});
-
-function onConnectedHandler (addr, port) {
-    console.log(`* Connected to ${addr}:${port}`);
-}
+main();
--- commands/channels.js
+++ commands/channels.js
@@ -7,19 +7,19 @@ const channels = (client, target, command, context) => {
             case 'add':
             case 'enter':
             case 'join':
-                if (authorizedUsers.includes(context['username'])) {
+                if (authorizedUsers.includes(context.userName)) {
                     if (args[2]) {
                         client.join(args[2]).then((val) => {
                             const joinedChannel = val[0].replace('#', '');
                             client.say(target, `I have joined ${joinedChannel}`);
                         });
                     } else {
-                        client.say(target, `${context['display-name']} You forgot to enter a channel name FailFish`);
+                        client.say(target, `${context.displayName} You forgot to enter a channel name FailFish`);
                     }
                 }
                 break;
             case 'list':
-                if (authorizedUsers.includes(context['username'])) {
+                if (authorizedUsers.includes(context.userName)) {
                     const list = client.getChannels();
                     list.forEach((val, index, array) => {
                         array[index] = val.replace('#', '');
@@ -31,7 +31,7 @@ const channels = (client, target, command, context) => {
             case 'leave':
             case 'part':
             case 'remove':
-                if (authorizedUsers.includes(context['username'])) {
+                if (authorizedUsers.includes(context.userName)) {
                     if (args[2]) {
                         const list = client.getChannels();
                         if (list.includes('#' + args[2].toLowerCase())) {
@@ -41,7 +41,7 @@ const channels = (client, target, command, context) => {
                             });
                         }
                     } else {
-                        client.say(target, `${context['display-name']} You forgot to enter a channel name FailFish`);
+                        client.say(target, `${context.displayName} You forgot to enter a channel name FailFish`);
                     }
                 }
                 break;
