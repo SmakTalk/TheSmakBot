a9f8cc8407c4d715208bfbec3afd620a6c328d83
Sahil Makhijani <svmakhijani94@gmail.com>
2023-04-11 01:34:34 -0700

    Auto greetings for raiders & new stream chatters

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const { RefreshingAuthProvider } = require('@twurple/auth');
+const { ApiClient } = require('@twurple/api');
 const { ChatClient } = require('@twurple/chat');
+const { EventSubWsListener } = require('@twurple/eventsub-ws');
 const smakapi = require('./api/smakapi.js');
-const whisperChat = require('./api/twitchapi.js');
+// const whisperChat = require('./api/twitchapi.js');
 const command = require('./commands');
+const Autogreet = require('./constants/autogreet.js');
 const http = require('./constants/http.js');
+let { greetedUsers, raiders } = require('./constants/users.js');
 
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
@@ -20,6 +24,10 @@ const main = async () => {
         }
     );
     authProvider.addUser(userId, JSON.parse(await smakapi('/token', http.GET)), ['chat']);
+    const apiClient = new ApiClient({ authProvider });
+
+    const listener = new EventSubWsListener({ apiClient });
+    listener.start();
 
     const client = new ChatClient({ authProvider, channels: [ process.env.CHANNEL_NAME ] });
 
@@ -28,36 +36,45 @@ const main = async () => {
     // whisperChat(authProvider, client);
 
     client.onMessage(async (channel, user, text, msg) => {
-        if (user === 'TheSmakBot') { return; }
+        if (user === 'thesmakbot') { return; }
       
+        const context = msg.userInfo;
         const commandName = text.trim();
     
         if (commandName.startsWith('$')) {
             switch (commandName.split(' ')[0]) {
                 case '$channel':
-                    command.channels(client, channel, commandName, msg.userInfo);
+                    command.channels(client, channel, commandName, context);
                     break;
                 case '$enter':
-                    command.entries(client, channel, commandName, msg.userInfo);
+                    command.entries(client, channel, commandName, context);
                     break;
                 case '$raid':
                     command.raids(commandName);
                     break;
                 case '$drawing':
-                    command.entries(client, channel, commandName, msg.userInfo);
+                    command.entries(client, channel, commandName, context);
                     break;
                 case '$streamer':
                     command.streamers(commandName);
                     break;
                 default:
-                    command.general(client, channel, commandName, msg.userInfo);
+                    command.general(client, channel, commandName, context);
+            }
+        }
+
+        if (user !== 'StreamElements') {
+            if (raiders.includes(context.displayName)) {
+                command.auto(Autogreet.RAIDER, client, channel, context.displayName);
+            } else if (greetedUsers[context.displayName] !== 1) {
+                command.auto(Autogreet.FIRST, client, channel, context.displayName);
             }
         }
     });
 
     client.onRitual((channel, user, ritualInfo, msg) => {
         if (ritualInfo.ritualName === 'new_chatter') {
-            command.auto('new', client, channel, user);
+            command.auto(Autogreet.NEW, client, channel, context.displayName);
         }
     });
 
@@ -69,6 +86,14 @@ const main = async () => {
         console.log(text);
         console.log(retryCount);
         console.log('------------');
+    });
+
+    listener.onStreamOffline(userId, e => {
+        greetedUsers = {};
+    });
+
+    listener.onChannelRaidTo(userId, e => {
+        raiders.push(e.raidingBroadcasterDisplayName);
     });
 };
 
--- commands/auto.js
+++ commands/auto.js
@@ -1,6 +1,21 @@
+const { greetedUsers, raiders } = require('../constants/users.js');
+
 const auto = (condition, client, channel, user) => {
-    if (condition === 'new') {
-        client.say(channel, `Welcome in @${user} <3`);
+    switch (condition) {
+        case 'new':
+            client.say(channel, `Welcome in @${user} <3`);
+            greetedUsers[user] = 1;
+            break;
+        case 'first':
+            client.say(channel, `Hello @${user} HeyGuys`);
+            greetedUsers[user] = 1;
+            break;
+        case 'raider':
+            client.say(channel, `Thank you so much for the raid @${user}! We really appreciate you bringing your community here!`);
+            const index = raiders.indexOf(user);
+            raiders.splice(index, 1);
+            greetedUsers[user] = 1;
+            break;
     }
 };
 
