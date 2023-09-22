052648ef7d83b213e0be252e6b4d640dd604de18
Sahil Makhijani <svmakhijani94@gmail.com>
2023-09-22 00:23:53 -0700

    Fixed user badge parsing & command permissions

--- commands/entries.js
+++ commands/entries.js
@@ -2,7 +2,7 @@ const authorizedUsers = require('../constants/authorized');
 
 let drawing;
 let entriesList = [];
-let isOpen = false;
+let isOpen = false; // drawing open for entries
 let winnersList = [];
 
 const entries = (client, channel, command, context) => {
@@ -10,7 +10,7 @@ const entries = (client, channel, command, context) => {
     switch (args[0]) {
         case '$enter':
             if (isOpen) {
-                const entry = (args[1] && (context.badges.has('moderator') || context.badges.has('broadcaster'))) ? args[1] : context.displayName;
+                const entry = (args[1] && (context.badges.has('moderator') || context.badges.has('broadcaster') || authorizedUsers.includes(context.userName))) ? args[1] : context.displayName;
                 if (entriesList.includes(entry)) {
                     client.say(channel, (args[1]) ? `${entry} has already entered the current drawing` : `${entry} You have already entered the current drawing`);
                 } else if (winnersList.includes(entry)) {
@@ -24,7 +24,7 @@ const entries = (client, channel, command, context) => {
             }
             break;
         case '$drawing':
-            if (context.badges.hasOwnProperty('moderator') || context.badges.hasOwnProperty('broadcaster')) {
+            if (context.badges.has('moderator') || context.badges.has('broadcaster') || authorizedUsers.includes(context.userName)) {
                 if (args[1]) {
                     switch (args[1]) {
                         case 'start':
@@ -85,7 +85,7 @@ const drawWinner = (client, target) => {
     const winner = entriesList[Math.floor(Math.random() * entriesList.length)];
     console.log('Entries: ' + entriesList);
     if (winner) {
-        client.say(target, `Congratulations @${winner}! You've won!`);
+        client.say(target, `Congratulations @${winner}! You've won the giveaway!`);
         winnersList.push(winner);
         entriesList.splice(entriesList.indexOf(winner), 1);
         console.log('Winners: ' + winnersList);
