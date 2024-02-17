49841d5b2d9e09d0e9f56b3d7ebc7c50d2f1642a
Sahil Makhijani <svmakhijani94@gmail.com>
2024-02-16 19:53:36 -0700

    Amended hug command again

--- commands/general.js
+++ commands/general.js
@@ -17,7 +17,7 @@ const general = async (client, channel, command, context) => {
             (args.length > 1) ? await client.say(channel, `Hi ${args[1]} HeyGuys`) : await client.say(channel, `Hi @${context.displayName} HeyGuys`);
             break;
         case '$hug':
-            if (args[1] && args[1].toLowerCase() === '@islandvibingpresents' || args[1].toLowerCase() === 'islandvibingpresents') {
+            if (args[1] && args[1].toLowerCase() === '@islandvibingpresents' || args[1].toLowerCase() === 'islandvibingpresents' || args[1].toLowerCase() === 'vibing') {
                 await client.say(channel, 'This is totally broken. 🧁');
             } else {
                 (args.length > 1) ? await client.action(channel, `${context.displayName} gives a big, friendly hug to ${args[1]} (but only if ${args[1]} accepts)`) : await client.action(channel, `gives a big, friendly hug to @${context.displayName}`);
